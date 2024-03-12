552f5eddf7574d219ae2697412269aa1c8b11cdf
willC707 <will.coleman707@gmail.com>
2024-03-11 23:56:25 -0400

    integration update

--- homeassistant/components/lch/__init__.py
+++ homeassistant/components/lch/__init__.py
@@ -2,11 +2,14 @@
 
 from __future__ import annotations
 
+import serial
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
+
+
 
 # TODO List the platforms that you want to support.
 # For your initial PR, limit it to 1 platform.
@@ -17,10 +20,15 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Light Control Hub from a config entry."""
 
     hass.data.setdefault(DOMAIN, {})
-    # TODO 1. Create API instance
-    # TODO 2. Validate the API connection (and authentication)
-    # TODO 3. Store an API object for your platforms to access
-    # hass.data[DOMAIN][entry.entry_id] = MyApi(...)
+    
+    try:
+        ser = serial.Serial(entry.data["port"])
+        print(f"Using port {ser.name}")
+    except serial.SerialException as e:
+        print(f"Error opening serial port: {e}")
+        return False
+    
+    hass.data[DOMAIN][entry.entry_id] = ser
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
